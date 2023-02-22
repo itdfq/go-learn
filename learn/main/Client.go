2fe8981409dcebc919817920dd72710463130d0c
duanfangqin <fangqin.duan@epean.com.cn>
2023-02-22 14:56:23 +0800

    feat:main包目录学习

---
+++ learn/main/Client.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"test/learn/main/pageTest"
+)
+
+func main() {
+	s := "test"
+	pageTest.Read(s)
+
+}
---
+++ learn/main/pageTest/demo.go
@@ -0,0 +1,12 @@
+package pageTest
+
+import "fmt"
+
+func Read(s string) {
+	fmt.Printf(s)
+}
+
+func readTest(s string) {
+	fmt.Printf("小写方法名：%s", s)
+
+}
