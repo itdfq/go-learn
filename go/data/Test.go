171ad3afc303a5631f20e5c7ac85002341a33199
duanfangqin <fangqin.duan@epean.com.cn>
2023-03-07 15:06:27 +0800

    feat:channel加深印象

---
+++ go/data/Test.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b float64 = float64(a)
+	fmt.Println(b)
+}
--- go/goroutine/ChannelTest.go
+++ go/goroutine/ChannelTest.go
@@ -15,18 +15,31 @@ func sum(s []int, c chan int) {
 	}
 	c <- sum //把值发送到通道
 }
+func subtraction(s []int, c chan int) {
+	sum := 100
+	for _, v := range s {
+		sum -= v
+
+	}
+	c <- sum
+}
 func main() {
 	s := []int{7, 2, 8, -9, 4, 0}
+	//默认情况下，通道是不带缓冲区的。发送端发送数据，同时必须有接收端相应的接收数据。
 	c := make(chan int) //声明通道
 	//执行 0 1 2
 	fmt.Println("len(s)/2=", len(s)/2, "s[:len(s)/2]=", s[:len(s)/2])
+	//i 取值：0，1，2
 	go sum(s[:len(s)/2], c)
-	//执行 2-最后
+	//执行 2-最后 i取值 3，4，5
 	go sum(s[len(s)/2:], c)
+	go subtraction(s, c)
 
 	//x, y := <-c, <-c
 	//fmt.Println(x, y, x+y)
 	x := <-c
 	y := <-c
+	//顺序是无法保证的
 	println(x, y)
+	println(<-c)
 }
