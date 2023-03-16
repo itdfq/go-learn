699e5fe14db6498d152b9f68c42977512b95e883
duanfangqin <fangqin.duan@epean.com.cn>
2023-03-16 17:12:53 +0800

    feat:多个单元测试测试

--- go/test/splict_test.go
+++ go/test/splict_test.go
@@ -21,3 +21,48 @@ func TestMoreSplit(t *testing.T) {
 		t.Errorf("excepted:%v, got:%v", want, got)
 	}
 }
+
+func TestMoreChineseSplit(t *testing.T) {
+	// 定义一个测试用例类型
+	type test struct {
+		input string
+		sep   string
+		want  []string
+	}
+	// 定义一个存储测试用例的切片
+	tests := []test{
+		{input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+		{input: "a:b:c", sep: ",", want: []string{"a:b:c"}},
+		//{input: "abcd", sep: "bc", want: []string{"a", "d"}},
+		{input: "枯藤老树昏鸦", sep: "老", want: []string{"枯藤", "树昏鸦"}},
+	}
+	// 遍历切片，逐一执行测试用例
+	for _, tc := range tests {
+		got := Splict(tc.input, tc.sep)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("excepted:%v, got:%v", tc.want, got)
+		}
+	}
+}
+
+func TestMoreNameSplict(t *testing.T) {
+	type test struct { // 定义test结构体
+		input string
+		sep   string
+		want  []string
+	}
+	tests := map[string]test{ // 测试用例使用map存储
+		"simple":      {input: "a:b:c", sep: ":", want: []string{"a", "b", "c"}},
+		"wrong sep":   {input: "a:b:c", sep: ",", want: []string{"a:b:c"}},
+		"more sep":    {input: "abcd", sep: "bc", want: []string{"a", "d"}},
+		"leading sep": {input: "枯藤老树昏鸦", sep: "老", want: []string{"枯藤", "树昏鸦"}},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) { // 使用t.Run()执行子测试
+			got := Splict(tc.input, tc.sep)
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("excepted:%#v, got:%#v", tc.want, got)
+			}
+		})
+	}
+}
