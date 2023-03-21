4a08afe0ff0bccf86ba69eae8bb1991f9c323195
duanfangqin <fangqin.duan@epean.com.cn>
2023-03-21 10:53:10 +0800

    feat：切片复习

--- go/array/SliceTest.go
+++ go/array/SliceTest.go
@@ -7,43 +7,45 @@ import "fmt"
 func main() {
 	/* 创建切片 */
 	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
-	printSlice(numbers)
+	printSlice(numbers) //len=9 cap=9 slice=[0 1 2 3 4 5 6 7 8]
+
+	printSlice(numbers[:]) //len=9 cap=9 slice=[0 1 2 3 4 5 6 7 8]
 
 	/* 打印原始切片 */
-	fmt.Println("numbers ==", numbers)
+	fmt.Println("numbers ==", numbers) //numbers == [0 1 2 3 4 5 6 7 8]
 
 	/* 打印子切片从索引1(包含) 到索引4(不包含)*/
-	fmt.Println("numbers[1:4] ==", numbers[1:4])
+	fmt.Println("numbers[1:4] ==", numbers[1:4]) //numbers[1:4] == [1 2 3]
 
 	/* 默认下限为 0*/
-	fmt.Println("numbers[:3] ==", numbers[:3])
+	fmt.Println("numbers[:3] ==", numbers[:3]) //numbers[:3] == [0 1 2]
 
 	/* 默认上限为 len(s)*/
-	fmt.Println("numbers[4:] ==", numbers[4:])
+	fmt.Println("numbers[4:] ==", numbers[4:]) //numbers[4:] == [4 5 6 7 8]
 
 	numbers1 := make([]int, 0, 5)
-	printSlice(numbers1)
+	printSlice(numbers1) //len=0 cap=5 slice=[]
 
 	/* 打印子切片从索引  0(包含) 到索引 2(不包含) */
 	number2 := numbers[:2]
-	printSlice(number2)
+	printSlice(number2) //len=2 cap=9 slice=[0 1]
 
 	/* 打印子切片从索引 2(包含) 到索引 5(不包含) */
 	number3 := numbers[2:5]
-	printSlice(number3)
+	printSlice(number3) // len=3 cap=7 slice=[2 3 4]
 	/* 切片追加元素*/
 	numbers = append(numbers, 250)
-	printSlice(numbers)
+	printSlice(numbers) //len=10 cap=18 slice=[0 1 2 3 4 5 6 7 8 250]
 	/* 同时添加多个元素 */
 	numbers = append(numbers, 2, 3, 4)
-	printSlice(numbers)
+	printSlice(numbers) //len=13 cap=18 slice=[0 1 2 3 4 5 6 7 8 250 2 3 4]
 
 	/* 创建切片 numbers1 是之前切片的两倍容量*/
 	numbersx := make([]int, len(numbers), (cap(numbers))*2)
 
 	/* 拷贝 numbers 的内容到 numbers1 */
 	copy(numbersx, numbers)
-	printSlice(numbersx)
+	printSlice(numbersx) //len=13 cap=36 slice=[0 1 2 3 4 5 6 7 8 250 2 3 4]
 
 }
 func printSlice(x []int) {
