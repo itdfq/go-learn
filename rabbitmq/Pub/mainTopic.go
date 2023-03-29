320c171c89d221bb0041ac6e6def2094281e4122
duanfangqin <fangqin.duan@epean.com.cn>
2023-03-29 17:28:01 +0800

    feat：rabbitmq TopPic 模式

---
+++ rabbitmq/Pub/mainTopic.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"go-learn/rabbitmq/RabbitMQ"
+	"strconv"
+	"time"
+)
+
+func main() {
+	kutengOne := RabbitMQ.NewRabbitMQTopic("exKutengTopic", "kuteng.topic.one")
+	kutengTwo := RabbitMQ.NewRabbitMQTopic("exKutengTopic", "kuteng.topic.two")
+	for i := 0; i <= 100; i++ {
+		kutengOne.PublishTopic("Hello kuteng topic one!" + strconv.Itoa(i))
+		kutengTwo.PublishTopic("Hello kuteng topic Two!" + strconv.Itoa(i))
+		time.Sleep(1 * time.Second)
+		fmt.Println(i)
+	}
+}
