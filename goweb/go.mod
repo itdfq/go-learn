b88e34c96d06f13f79a65f2818b9ca3a3bda3988
duanfangqin <fangqin.duan@epean.com.cn>
2023-04-14 12:28:30 +0800

    feat:【bee】多个配置文件设置

--- goweb/go.mod
+++ goweb/go.mod
@@ -9,5 +9,4 @@ require (
 	github.com/shiena/ansicolor v0.0.0-20200904210342-c7312218db18 // indirect
 	github.com/smartystreets/goconvey v1.6.4
 	google.golang.org/appengine v1.6.7 // indirect
-	gorm.io/gorm v1.25.0
 )
--- goweb/main.go
+++ goweb/main.go
@@ -5,12 +5,16 @@ import (
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 	_ "goweb/routers"
+	"log"
 )
 
 func main() {
 
 	beego.Run()
 }
+
+const COLON = ":"
+
 func init() {
 	//初始化数据库连接
 	// 这里注册一个default默认数据库，数据库驱动是mysql.
@@ -20,6 +24,18 @@ func init() {
 	//      password    - mysql密码
 	//      db_name     - 数据库名
 	//      127.0.0.1:3306 - 数据库的地址和端口
-	orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/go?charset=utf8")
+
+	//通过这种方式获取配置信息
+	mysqlUser := beego.AppConfig.String("mysqlUser")
+	mysqlPasswd := beego.AppConfig.String("mysqlPasswd")
+	mysqlUrls := beego.AppConfig.String("mysqlUrls")
+	dbName := beego.AppConfig.String("mysqlDB")
+	mysqlProt := beego.AppConfig.String("mysqlPort")
+	dataSource := mysqlUser + COLON + mysqlPasswd + "@tcp(" + mysqlUrls + COLON + mysqlProt + ")/" + dbName + "?charset=utf8"
+	orm.RegisterDataBase("default", "mysql", dataSource)
+
+	log.Println("数据库初始化成功,当前连接的数据库为：", dbName)
+	runmode := beego.AppConfig.String("runmode")
+	log.Println("系统启动成功,当前环境为：{" + runmode + "}")
 
 }
