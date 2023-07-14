d0762a51cdd6bcd41d7335cc45852d019e695ae2
GodChin <909256107@qq.com>
2023-07-14 10:22:03 +0800

    feat: go的map测试

--- go/array/mapTest.go
+++ go/array/mapTest.go
@@ -13,6 +13,23 @@ func main() {
 		"orange": 3,
 	}
 
+	n := map[string]int{
+		"abe":    0,
+		"orange": 1,
+		"absd":   2,
+	}
+	keys1 := make([]string, 0, len(n))
+	for k := range n {
+		keys1 = append(keys1, k)
+	}
+
+	//对切片进行排序
+	sort.Strings(keys1)
+
+	for _, k := range keys1 {
+		fmt.Printf("key=%s,value= %d \n", k, n[k])
+	}
+
 	// 将 map 中的键存储到切片中
 	keys := make([]string, 0, len(m))
 	for k := range m {
